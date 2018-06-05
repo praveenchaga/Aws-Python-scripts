02d8e09371f73edc5518e250e19bf705ca33413e
Praveen <chagapraveen@gmail.com>
2018-06-05 22:02:50 +0530

    Update startrds.py

--- startrds.py
+++ startrds.py
@@ -1,3 +1,11 @@
+############################################################################################################################
+#   To Start specific RDS_ Instance every day @ 11:30 AM IST Automatically in Aws using server less computing (Aws-Lambda)
+#   Create IAM role with RDS-full access.
+#   Create Lambda function
+#   Create cloudwatdh event using then Cron expression 0 6 ? * MON-FRI *
+#  
+#   PRAVEEN KUMAR.CH                     chagapraveen@gmail.com                                       Date: 05-06-2018
+############################################################################################################################
 import boto3
 def lambda_handler(event, context):
     RDS = boto3.client('rds')    
